5ad336550dbde712802fd2bdfb00a2bb7c8d466a
kamilkn <61585695+kamilkn@users.noreply.github.com>
2023-10-29 14:07:49 +0300

    add logging, add simple PoW algorithm

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tokio::net::TcpStream;
 use tokio::io::{AsyncWriteExt, AsyncReadExt};
+use sha2::Digest;
+use std::time::{Instant};
 
 #[tokio::main]
 async fn main() {
@@ -7,14 +9,33 @@ async fn main() {
   let mut buf = [0; 1024];
   let n = stream.read(&mut buf).await.unwrap();
   let challenge = String::from_utf8_lossy(&buf[..n]).to_string();
-  let nonce = solve_challenge(&challenge);  // Assume this function is implemented
+  println!("Received challenge: {}", challenge);
+  let nonce = solve_challenge(&challenge);
   stream.write_all(nonce.as_bytes()).await.unwrap();
   let n = stream.read(&mut buf).await.unwrap();
   let quote = String::from_utf8_lossy(&buf[..n]).to_string();
-  println!("{}", quote);
+  println!("Received quote: {}", quote);
 }
 
-fn solve_challenge(_challenge: &str) -> String {
-  // Placeholder, implement PoW solving algorithm
-  "some-nonce".to_string()
+fn solve_challenge(challenge: &str) -> String {
+  // Assume the challenge format is "random_string:00000"
+  let parts: Vec<&str> = challenge.split(':').collect();
+  let random_string = parts[0];
+  let mut nonce = 0;
+
+  let start_time = Instant::now();
+
+  loop {
+      let attempt = format!("{}{}", random_string, nonce);
+      let hash = sha2::Sha256::digest(attempt.as_bytes());
+      if hash.starts_with(b"000") {  // Assuming the requirement is 5 leading zeros
+          let duration = start_time.elapsed();
+          println!("Затраченное время: {:?}", duration); 
+          return nonce.to_string();
+      }
+      if nonce % 10_000 == 0 {  // Логирование каждые 10,000 итераций
+          println!("Current nonce: {}, Hash: {:?}", nonce, hash);
+      }
+      nonce += 1;
+  }
 }
